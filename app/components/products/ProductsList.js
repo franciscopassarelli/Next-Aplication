dc66b954dc47f754f3e1b757ad3b03c95aa8b8c6
Francisco <franciscopassarelli7@gmail.com>
2024-03-07 16:49:42 -0300

    deploy-next-js

--- app/components/products/ProductsList.js
+++ app/components/products/ProductsList.js
@@ -3,30 +3,34 @@ import ProductCard from "./ProductCard";
 const ProductsList = async ({ categoria }) => {
     try {
         const response = await fetch(`${process.env.API_URL}/api/productos/${categoria}`, {
-            cache: "no-store",
-            next: { tags: ["productos"] },
-            headers: { 'Content-Type': 'application/json' }  
-        })
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'X-Next-Tags': 'productos'  // Encabezado personalizado para enviar información adicional
+            },
+            cache: 'no-store'
+        });
+
         if (!response.ok) {
             throw new Error(`HTTP error! Status: ${response.status}`);
         }
+
         const items = await response.json();
+
         if (items) {
             return (
                 <section className="container m-auto flex justify-center items-center gap-12 flex-wrap">
                     {items.map(item => <ProductCard key={item.slug} item={item} />)}
                 </section>
-            )
+            );
         } else {
             console.error('Cadena JSON vacía o indefinida.');
-            return <p>No hay productos para esta categoria.</p>;
+            return <p>No hay productos para esta categoría.</p>;
         }
     } catch (error) {
         console.error('Error al obtener datos:', error);
+        return <p>Error al obtener datos.</p>;
     }
+};
 
-    return <p>No hay productos</p>;
-}
-
-
-export default ProductsList
+export default ProductsList;
--- app/firebase/config.js
+++ app/firebase/config.js
@@ -1,17 +1,7 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore"
-import { getStorage } from "firebase/storage" 
-import { getAuth, GoogleAuthProvider } from "firebase/auth"
-/*
-const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-};
-*/
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBv9PvXj3xsd6gmDpIyRU2pM3zduIxKXOE",
@@ -22,20 +12,9 @@ const firebaseConfig = {
   appId: "1:516632641361:web:0d4d3b734a60060a569d70"
 };
 
-
-// Initialize Firebase
+// Inicializa Firebase
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app)
-export const storage = getStorage(app)
-export const auth = getAuth(app)
-export const provider = new GoogleAuthProvider()
-
-
-/*
-NEXT_PUBLIC_FIREBASE_API_KEY=AIzaSyBv9PvXj3xsd6gmDpIyRU2pM3zduIxKXOE
-NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN=codernext-app-b43fb.firebaseapp.com
-NEXT_PUBLIC_FIREBASE_PROJECT_ID=codernext-app-b43fb
-NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET=codernext-app-b43fb.appspot.com
-NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID=516632641361
-NEXT_PUBLIC_FIREBASE_APP_ID=1:516632641361:web:0d4d3b734a60060a569d70
-*/
+export const db = getFirestore(app);
+export const storage = getStorage(app);
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
