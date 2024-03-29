46811afc4fe712b392d8003cd151f82e9ea16437
Francisco <franciscopassarelli7@gmail.com>
2024-03-29 18:50:48 -0300

    formdelete2

--- app/Contacto/page.js
+++ app/Contacto/page.js
@@ -1,18 +1,83 @@
 "use client"
-import React from 'react';
-import ContactForm from '../components/Contacto/ContactForm';
-const Contacto = () => {
+import { useState } from "react"
+import Boton from "../components/ui/Boton";
+import { useRouter } from "next/navigation";
+
+// Firebase
+import { db } from "@/app/firebase/config";
+import { setDoc, doc, Timestamp, getDoc, writeBatch } from "firebase/firestore";
+
+ const ContactForm= () => {
+    const router = useRouter()
+    const [values, setValues] = useState({
+        email: "",
+        text: ""
+    })
+
+    const handleChange = (e) => {
+        setValues({
+            ...values,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        let url = "api/contacto"
+
+        await fetch(url, {
+            method: "POST",
+            body: JSON.stringify(values)
+        })
+
+        const docId = Timestamp.fromDate(new Date()).toMillis()
+        const orderRef = doc(db, "mensajes", String(docId))
+        await setDoc(orderRef, values)
+
+        router.push("/thankscontact")
+    }
+    
+
   return (
-    <div className="bg-blue-800 min-h-screen flex flex-col">
-      <main className="container mx-auto flex-grow p-4 bg-gradient-to-r from-sky-800 to-gray-900">
-        <div className="bg-white bg-opacity-75 rounded p-6">
-          <h1 className="text-4xl text-gray-900 font-bold mb-4">Contacto</h1>
-          <hr />
-          <br />
-          <ContactForm />
-        </div>
-      </main>
-    </div>
+    <div className="w-full max-w-md mx-auto">
+    <form onSubmit={handleSubmit} className="bg-white bg-opacity-75 shadow-md rounded px-8 pt-6 pb-8 mb-4">
+      <div className="mb-4">
+        <label htmlFor="email" className="block text-gray-700 text-sm font-bold mb-2">
+          Email
+        </label>
+        <input
+        id="email"
+          type="email"
+          required
+          placeholder="Tu email"
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          name="email"
+          onChange={handleChange}
+        />
+  
+        <label htmlFor="message" className="block text-gray-700 text-sm font-bold mb-2">
+          Dejanos un mensaje
+        </label>
+        <textarea
+          id="message"
+          rows="4"
+          placeholder="Déjanos tu mensaje"
+          className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+          name="text"
+          onChange={handleChange}
+        />
+  
+        <Boton
+          type="submit"
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+        >
+          Enviar
+        </Boton>
+      </div>
+    </form>
+  </div>
+  
   );
 };
-export default Contacto
+
+export default ContactForm;
